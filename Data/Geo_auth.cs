71ba1f929f8518a5964c7c3f3f3971631ba424e4
Vova <User@DESKTOP-0DN4EHA>
2026-03-22 13:30:54 +0300

    add geo_auth

---
+++ Data/Geo_auth.cs
@@ -0,0 +1,11 @@
+﻿namespace LabWork20.Data;
+
+public class Geo_auth
+{
+    public string? Geo { get; set; }
+
+    public string? Login { get; set; }
+    public string? Password { get; set; }
+
+    public void GetUserLocation() => Console.WriteLine(Geo);
+}
